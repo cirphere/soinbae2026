db0ea43f56101d4ef1b78bebb7f40ff0f5a3a477
김도형 <rlaehgud0194@gmail.com>
2026-02-28 18:42:43 +0900

    fix: meta data 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,29 @@ import { Providers } from "@/components/providers";
 import "./globals.css";
 
 export const metadata = {
-  title: "Soinbae Next",
-  description: "Next.js application",
+  title: "SoInBae",
+  description: "전남대학교 인공지능학부 동아리 AIM 소인배 팀 소개 사이트",
+  icons: {
+    icon: "/logo.svg",
+  },
+  author: "김도형",
+  openGraph: {
+    type: "website",
+    url: "https://soinbae.dohyeong1203.com",
+    title: "SoInBae",
+    description: "전남대학교 인공지능학부 동아리 AIM 소인배 팀 소개 사이트",
+    images: [
+      {
+        url: "/soinbae.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SoInBae",
+    description: "전남대학교 인공지능학부 동아리 AIM 소인배 팀 소개 사이트",
+    images: ["/soinbae.png"],
+  },
 };
 
 export default function RootLayout({
